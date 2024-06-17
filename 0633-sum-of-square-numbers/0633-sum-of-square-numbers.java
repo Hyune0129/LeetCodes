8cd1a7964a70df9b869e38be4d61fd3728aa54d3
Hyune0129 <97016146+Hyune0129@users.noreply.github.com>
2024-06-17 14:37:01 +0900

    Time: 1019 ms (5.11%), Space: 44.3 MB (5.18%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        if(c == 0)
+            return true;
+        for(int a=0; Math.pow(a, 2) <= c; a++) {
+            double temp = Math.sqrt(c - Math.pow(a,2));
+            System.out.println(temp);
+            if(temp == (int)temp) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
