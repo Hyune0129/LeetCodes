0288fddbc6d97e89d481bdf5a650d398cfff3396
Hyune0129 <97016146+Hyune0129@users.noreply.github.com>
2024-08-05 12:31:13 +0900

    Time: 7 ms (31.23%), Space: 49.5 MB (15.39%) - LeetHub

---
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -0,0 +1,24 @@
+// ref : https://leetcode.com/problems/maximum-sum-circular-subarray/solutions/3066636/weird-kadane-explanation-with-images/
+
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int n = nums.length;
+        int minSum = nums[0];
+        int maxSum = nums[0];
+        int maxSub = nums[0];
+        int minSub = nums[0];
+        int totalSum = nums[0];
+        for(int i=1; i<n; i++) {
+            minSum = Math.min(nums[i], nums[i] + minSum);
+            maxSum = Math.max(nums[i], nums[i] + maxSum);
+            maxSub = Math.max(maxSub, maxSum);
+            minSub = Math.min(minSub, minSum);
+            totalSum += nums[i];
+        }
+        if(maxSub <= 0) { // [-3, -2, -3]
+            return maxSub;
+        }
+        
+        return Math.max(totalSum - minSub, maxSub);
+    }
+}
