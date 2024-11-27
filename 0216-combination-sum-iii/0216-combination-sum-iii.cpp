28c4927557f53562231b2e82d324ba30b2208546
Hyune0129 <97016146+Hyune0129@users.noreply.github.com>
2024-11-27 12:01:27 +0900

    Time: 2 ms (28.88%), Space: 8.8 MB (26.58%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    vector<vector<int>> ans;
+    int _k;
+    int _n;
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        _k = k;
+        _n = n;
+        vector<int> comb;
+        backTracking(comb, 0, 0);
+        return ans;
+    }
+
+    void backTracking(vector<int>& combination, int depth, int usedNum) {
+        if(depth == _k) {
+            int sum = accumulate(combination.begin(), combination.end(), 0);
+            if(sum == _n)
+            {
+                ans.push_back(vector<int>(combination.begin(), combination.end()));
+            }
+            return;
+        }
+        for(int i=usedNum + 1; i<=9; i++) {
+            combination.push_back(i);
+            backTracking(combination, depth + 1, i);
+            combination.pop_back();
+        }
+
+    }
+
+};
